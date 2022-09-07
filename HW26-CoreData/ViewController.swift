b36fb4e5d7f6bb7a5a43aeb79d42d85af5bfd61a
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-09-07 12:06:45 +0300

    Initial Commit

---
+++ HW26-CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW26-CoreData
+//
+//  Created by Anatoliy on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
